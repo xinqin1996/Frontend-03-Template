19b18bc8839c7a3d98edd9f66760c868a6850c28
xinqin1996 <1923755199@qq.com>
2020-11-26 19:33:23 +0800

    qin

--- week14/jsx/webpack.config.js
+++ week14/jsx/webpack.config.js
@@ -1,5 +1,12 @@
+const path = require('path')
+
 module.exports = {
   entry: "./animation-demo.js",
+  output: {
+    filename: 'animation-demo.js', // 必须设置output，默认输出到mian.js
+    path: path.resolve(__dirname, 'dist'),
+  },
+  // devtool: 'source-map', // 启动调试模式
   module: {
     rules: [
       {
@@ -14,8 +21,10 @@ module.exports = {
       }
     ]
   },
-  mode: "development",
-  // devServer: {
-  //   contentBase: './dist'
-  // }
+  mode: "development", // 开发模式
+  devtool: 'source-map', // 启动调试
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  }
 }
