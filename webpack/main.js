a07df6d795d5c4ef39ff907094b6aa71986fce77
xinqin1996 <1923755199@qq.com>
2020-11-26 19:40:11 +0800

    qin

---
+++ webpack/main.js
@@ -0,0 +1,2 @@
+/** 手动轮播，优化，只改变特定div的位置 */
+console.log("main.js")
---
+++ webpack/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path')
+
+module.exports = {
+  entry: "./main.js",
+  output: {
+    filename: 'main.js', // 必须设置output，默认输出到mian.js
+    path: path.resolve(__dirname, 'dist'),
+  },
+  // devtool: 'source-map', // 启动调试模式
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [["@babel/plugin-transform-react-jsx", { pragma: "createElement" }]]
+          }
+        }
+      }
+    ]
+  },
+  mode: "development", // 开发模式
+  devtool: 'source-map', // 启动调试
+  devServer: {
+    contentBase: './dist',
+    hot: true,
+  }
+}
