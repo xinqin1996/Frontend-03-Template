643e6d32938fe1a69467eb0c255ad67d8c1f104f
xinqin1996 <1923755199@qq.com>
2020-11-27 10:47:09 +0800

    qin

--- week14/jsx/animation-demo.js
+++ week14/jsx/animation-demo.js
@@ -1,10 +1,17 @@
 import { TimeLine, Animation } from "./animation"
 
-console.log(123123)
+// 创建时间线
 let t1 = new TimeLine();
 
-let ani = new Animation(document.querySelector("#el").style, transform, 1, 100, 1000, null, (v) => { return `translateX(${v}px)` })
+// object, property, startVal, endVal, duration, delay, timingFunction, template
+let animation = new Animation(document.querySelector("#el").style, "transform", 1, 500, 4000, 0, null, (v) => { return `translateX(${v}px)` })
 
-time.start()
+// 启动时间线
+t1.start()
 
-t1.add(ani) // 运行后执行add操作
+// 添加动画
+t1.add(animation) // 运行后执行add操作
+
+// 点击事件
+document.querySelector("#pause-button").addEventListener("click", () => { t1.pause() })
+document.querySelector("#resume-button").addEventListener("click", () => { t1.resume() })
--- week14/jsx/animation.js
+++ week14/jsx/animation.js
@@ -2,6 +2,10 @@ const TICK = Symbol("tick")
 const TICK_HANDlER = Symbol("tick-handler")
 const ANIMATIONS = Symbol("animations")
 const START_TIME = Symbol("start-time")
+const PAUSE_START = Symbol("pause-start")
+const PAUSE_END = Symbol("pause-end")
+const PAUSE_TIME = Symbol("pause-time")
+
 
 /**
  * 时间线
@@ -14,13 +18,14 @@ export class TimeLine {
   constructor() {
     this[ANIMATIONS] = new Set() // 保存animation实例
     this[START_TIME] = new Map() // 保存animation被添加进this[ANIMATIONS]的时间
-
+    this[PAUSE_TIME] = 0 // 初始化暂停时间
   }
 
   /**
    * startTime TimeLine启动时间
    * now 执行到某一帧的时间
    * t 某一个动画执行时间
+   * this[TICK]在start中被初始化
    */
   start() {
     let startTime = Date.now();
@@ -29,11 +34,11 @@ export class TimeLine {
       for (let animation of this[ANIMATIONS]) {
         let t;
         if (this[START_TIME].get(animation) < startTime) { // TimeLine启动前添加的
-          t = now - startTime;
+          t = now - startTime - this[PAUSE_TIME];
         } else {
-          t = now - this[START_TIME].get(animation); // TimeLine启动后添加的，以添加进的时间为准          
+          t = now - this[START_TIME].get(animation) - this[PAUSE_TIME]; // TimeLine启动后添加的，以添加进的时间为准          
         }
-        if (animation.duration < t) {
+        if (animation.duration < t) { // animation.duration的进行完，删除该动画
           this[ANIMATIONS].delete(animation)
           t = animation.duration  // 防止范围超出
         }
@@ -44,11 +49,19 @@ export class TimeLine {
     this[TICK]()
   }
 
+  /**
+   * pause时保存暂停时间，
+   * resume保存继续时间,
+   * 两者相减就是暂停的市场
+   */
   pause() {
+    this[PAUSE_START] = Date.now()
     cancelAnimationFrame(this[TICK_HANDlER]) // 停止动画
   }
 
   resume() {
+    this[PAUSE_END] = Date.now()
+    this[PAUSE_TIME] = this[PAUSE_TIME] + this[PAUSE_END] - this[PAUSE_START];
     this[TICK]()
   }
 
@@ -63,12 +76,10 @@ export class TimeLine {
 
 /**
  * 属性动画
- * 起始值
- * 终止值
- * 
+ * receive函数:在TimeLine启动中，接受receive(time)，改变animation实例的属性
  */
 export class Animation {
-  constructor(object, property, startVal, endVal, duration, delay, timingFunction) {
+  constructor(object, property, startVal, endVal, duration, delay, timingFunction, template) {
     // 保存实例的属性
     this.object = object;
     this.property = property;
@@ -76,13 +87,13 @@ export class Animation {
     this.endVal = endVal;
     this.duration = duration;
     this.timingFunction = timingFunction;
-    this.delay = delay;
+    this.delay = delay; // 动画延迟启动
+    this.template = template;
   }
   // 接受虚拟时间，改变object的属性
   receive(time) {
-    // console.log(time)
     let range = this.endVal - this.startVal
-    this.object[this.property] = this.startVal + range * time / this.duration
-    console.log("属性变化", this.object[this.property])
+    this.object[this.property] = this.template(this.startVal + range * time / this.duration)
+    // console.log("属性变化", this.object[this.property])
   }
 }
